49dcdfde3d2627350d90ef594403fe68c2ee12f5
HAOPRONK123 <95024704+HAOPRONK123@users.noreply.github.com>
2023-03-07 23:43:07 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Pé Tép';
+const giftUrl = 'peTep.png';
+const eventName = 'Chúc mừng 8-3';
+const titleCard = 'Tặng Pé Tép';
+const contentCard = 'Chúc Pé ngày 8-3 sẽ tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với pé trong hôm nay và cả những ngày sau';
+const giftImage = 'peTep.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
